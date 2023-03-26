c0ff57d60ed2da075560d8fb500bd7a75b0e8e1e
promecarus <haikalslipi@gmail.com>
2023-03-26 08:29:41 +0700

    Save
    Explicit intent done

--- app/src/main/java/com/promecarus/mobileprogramming/Tugas02ExplicitIntentActivity.kt
+++ app/src/main/java/com/promecarus/mobileprogramming/Tugas02ExplicitIntentActivity.kt
@@ -1,5 +1,6 @@
 package com.promecarus.mobileprogramming
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.promecarus.mobileprogramming.databinding.ActivityTugas02ExplicitIntentBinding
@@ -11,5 +12,14 @@ class Tugas02ExplicitIntentActivity : AppCompatActivity() {
         binding = ActivityTugas02ExplicitIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
         title = "Tugas 2 | Explicit Intent"
+
+        binding.buttonSubmit.setOnClickListener {
+            val i = Intent(this, Tugas02ImplicitIntentActivity::class.java)
+                .putExtra("email", binding.editTextEmail.text.toString())
+                .putExtra("kodePos", binding.editTextPostalCode.text.toString())
+                .putExtra("nama", binding.editTextName.text.toString())
+                .putExtra("telepon", binding.editTextTelephone.text.toString())
+            startActivity(i)
+        }
     }
 }
